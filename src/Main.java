b8af275ce70eadab70630e020108a9efcd24e003
implodingocelot <think59@outlook.com>
2023-08-16 12:04:29 -0600

    i made the conjugator a fucking window i dont know how this works anymore. ddos still cmd only so its hacker-like

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ import java.util.*;
 import java.io.*;
 import java.net.*;
 import static java.lang.System.exit;
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -12,14 +17,16 @@ public class Main {
         int num;
         int personal;
         int catgirl;
-        boolean ddos;
+        int area = 0;
         boolean kill_switch = false;
         System.out.println("Enter root: ");
         root = reader.next();
         if (root.equals("ddos")) {
-            ddos = true;
-        } else {
-            ddos = false;
+            area = 1;
+        } else if (root.equals("WINDOW")){
+            area = 2;
+        }
+        else {
             if (root.endsWith("ar")) {
                 catgirl = 1;
             } else if (root.endsWith("er")) {
@@ -33,6 +40,11 @@ public class Main {
             num = reader.nextInt();
             System.out.println("Enter POV (1, 2, or 3): ");
             personal = reader.nextInt();
+            if(root.length() < 2)
+            {
+                System.out.print("not long enough LMAO");
+                exit(12);
+            }
             root = root.substring(0, root.length() - 2);
             if (num > 2) {
                 num = 2;
@@ -47,7 +59,7 @@ public class Main {
             root = root + ED[catgirl - 1][num - 1][personal - 1];
             System.out.println("Your answer is: " + root + ".");
         }
-        if(ddos)
+        if(area == 1)
         {
             String ipAddress;
             System.out.println("What IP would you like to ping?");
@@ -68,6 +80,15 @@ public class Main {
             }
 
         }
+        if(area == 2)
+        {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new VariableDisplaySwing();
+                }
+            });
+        }
     }
     public static void sendPingRequest(String ipAddress, int i)
             throws UnknownHostException, IOException
@@ -82,6 +103,90 @@ public class Main {
         }
         else {
             geek.isReachable(100);
+        }
+    }
+    public static class VariableDisplaySwing {
+
+        private JTextField variable1Field, variable2Field, variable3Field;
+        private JButton displayButton;
+        private JLabel resultLabel;
+
+        public VariableDisplaySwing() {
+            JFrame frame = new JFrame("Variable Display Window");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(300, 200);
+
+            JPanel panel = new JPanel();
+            panel.setLayout(new GridLayout(4, 2, 10, 10));
+
+            variable1Field = new JTextField();
+            variable2Field = new JTextField();
+            variable3Field = new JTextField();
+            displayButton = new JButton("Display");
+            resultLabel = new JLabel();
+
+            panel.add(new JLabel("Root:"));
+            panel.add(variable1Field);
+            panel.add(new JLabel("Personal (int):"));
+            panel.add(variable2Field);
+            panel.add(new JLabel("Number of People (int):"));
+            panel.add(variable3Field);
+            panel.add(displayButton);
+            panel.add(resultLabel);
+
+            displayButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    displaySelectedVariable();
+                }
+            });
+
+            frame.add(panel);
+            frame.setVisible(true);
+        }
+
+        private void displaySelectedVariable() {
+            String selectedValue = "";
+            int catgirl;
+            Scanner reader = new Scanner(System.in);
+            int personal = Integer.parseInt(variable2Field.getText());
+            int num = Integer.parseInt(variable3Field.getText());
+            if (!variable1Field.getText().isEmpty() && !variable2Field.getText().isEmpty() && !variable3Field.getText().isEmpty()) {
+
+
+                    if (variable1Field.getText().endsWith("ar")) {
+                        catgirl = 1;
+                    } else if (variable1Field.getText().endsWith("er")) {
+                        catgirl = 2;
+                    } else if (variable1Field.getText().endsWith("ir")) {
+                        catgirl = 3;
+                    } else {
+                        catgirl = 0;
+                    }
+                    if(variable1Field.getText().length() < 2)
+                    {
+                        System.out.print("not long enough LMAO");
+                        exit(12);
+                    }
+                    String helpMe = variable1Field.getText().substring(0, variable1Field.getText().length() - 2);
+                    if (num > 2) {
+                        num = 2;
+                    }
+                    // error codes
+                    if (catgirl == 0) {
+                        System.out.println("dumbo");
+                        exit(32767);
+                    }
+                    // end of error codes
+                    String[][][] ED = {{{"o", "as", "a"}, {"amos", "ais", "an"}}, {{"o", "es", "e"}, {"emos", "eis", "en"}}, {{"o", "es", "e"}, {"imos", "is", "en"}}};
+                    helpMe = helpMe + ED[catgirl - 1][num - 1][personal - 1];
+                    System.out.println("Your answer is: " + helpMe + ".");
+                    selectedValue = helpMe;
+            } else {
+                selectedValue = "Fill in all variables!";
+            }
+
+            resultLabel.setText("Selected variable: " + selectedValue);
         }
     }
 }
