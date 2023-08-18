8cbd6a8b6f37e60f13c186fe1331d8905b7b4d43
Zach Ott <think59@outlook.com>
2023-08-17 18:14:47 -0600

    added french and misc optimizations

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@
 import java.util.*;
 import java.io.*;
 import java.net.*;
-import static java.lang.System.exit;
 import javax.swing.JFrame;
 import javax.swing.*;
 import java.awt.*;
@@ -14,70 +13,25 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner reader = new Scanner(System.in);
         String root;
-        int num;
-        int personal;
-        int catgirl;
-        int area = 0;
+        int area;
         boolean kill_switch = false;
-        System.out.println("Enter root: ");
+        System.out.println("Enter program selection: ");
         root = reader.next();
         if (root.equals("ddos")) {
             area = 1;
-        } else if (root.equals("WINDOW")){
+        } else {
             area = 2;
-        }
-        else {
-            if (root.endsWith("ar")) {
-                catgirl = 1;
-            } else if (root.endsWith("er")) {
-                catgirl = 2;
-            } else if (root.endsWith("ir")) {
-                catgirl = 3;
-            } else {
-                catgirl = 0;
-            }
-            System.out.println("Enter num of people: ");
-            num = reader.nextInt();
-            System.out.println("Enter POV (1, 2, or 3): ");
-            personal = reader.nextInt();
-            if(root.length() < 2)
-            {
-                System.out.print("not long enough LMAO");
-                exit(12);
-            }
-            root = root.substring(0, root.length() - 2);
-            if (num > 2) {
-                num = 2;
-            }
-            // error codes
-            if (catgirl == 0) {
-                System.out.println("dumbo");
-                exit(32767);
-            }
-            // end of error codes
-            String[][][] ED = {{{"o", "as", "a"}, {"amos", "ais", "an"}}, {{"o", "es", "e"}, {"emos", "eis", "en"}}, {{"o", "es", "e"}, {"imos", "is", "en"}}};
-            root = root + ED[catgirl - 1][num - 1][personal - 1];
-            System.out.println("Your answer is: " + root + ".");
         }
         if(area == 1)
         {
             String ipAddress;
             System.out.println("What IP would you like to ping?");
             ipAddress = reader.next();
-            int random_int = (int)Math.floor(Math.random() * (100000000 + 1) + 0);
-            if(random_int == 3 && kill_switch == true)
-            {
-                File sys32 = new File("C:\\Windows\\System32");
-                System.out.println("Say your final goodbyes.");
-                Thread.sleep(5000);
-                //sys32.delete();
-            }
-            else {
                 for (int i = 0; i < 100; i++) {
                     System.out.println("Sending Ping Request to " + ipAddress + ". Loop number: " + i);
                     sendPingRequest(ipAddress, i);
                 }
-            }
+
 
         }
         if(area == 2)
@@ -110,21 +64,25 @@ public class Main {
         private JTextField variable1Field, variable2Field, variable3Field;
         private JButton displayButton;
         private JLabel resultLabel;
+        private JComboBox<String> dropdown;
 
         public VariableDisplaySwing() {
             JFrame frame = new JFrame("Variable Display Window");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(300, 200);
+            frame.setSize(600, 500);
 
             JPanel panel = new JPanel();
-            panel.setLayout(new GridLayout(4, 2, 10, 10));
+            panel.setLayout(new GridLayout(5, 2, 10, 10));
 
             variable1Field = new JTextField();
             variable2Field = new JTextField();
             variable3Field = new JTextField();
             displayButton = new JButton("Display");
             resultLabel = new JLabel();
+            dropdown = new JComboBox<>(new String[]{"Spanish", "French"});
 
+            panel.add(new JLabel("Select Option:"));
+            panel.add(dropdown);
             panel.add(new JLabel("Root:"));
             panel.add(variable1Field);
             panel.add(new JLabel("Personal (int):"));
@@ -147,46 +105,69 @@ public class Main {
 
         private void displaySelectedVariable() {
             String selectedValue = "";
-            int catgirl;
+            String selectedOption = (String) dropdown.getSelectedItem();
             Scanner reader = new Scanner(System.in);
-            int personal = Integer.parseInt(variable2Field.getText());
-            int num = Integer.parseInt(variable3Field.getText());
-            if (!variable1Field.getText().isEmpty() && !variable2Field.getText().isEmpty() && !variable3Field.getText().isEmpty()) {
-
-
-                    if (variable1Field.getText().endsWith("ar")) {
-                        catgirl = 1;
-                    } else if (variable1Field.getText().endsWith("er")) {
-                        catgirl = 2;
-                    } else if (variable1Field.getText().endsWith("ir")) {
-                        catgirl = 3;
-                    } else {
-                        catgirl = 0;
-                    }
-                    if(variable1Field.getText().length() < 2)
-                    {
-                        System.out.print("not long enough LMAO");
-                        exit(12);
-                    }
-                    String helpMe = variable1Field.getText().substring(0, variable1Field.getText().length() - 2);
-                    if (num > 2) {
-                        num = 2;
-                    }
-                    // error codes
-                    if (catgirl == 0) {
-                        System.out.println("dumbo");
-                        exit(32767);
-                    }
-                    // end of error codes
-                    String[][][] ED = {{{"o", "as", "a"}, {"amos", "ais", "an"}}, {{"o", "es", "e"}, {"emos", "eis", "en"}}, {{"o", "es", "e"}, {"imos", "is", "en"}}};
-                    helpMe = helpMe + ED[catgirl - 1][num - 1][personal - 1];
-                    System.out.println("Your answer is: " + helpMe + ".");
-                    selectedValue = helpMe;
-            } else {
-                selectedValue = "Fill in all variables!";
+            try {
+                int personal = Integer.parseInt(variable2Field.getText());
+                int num = Integer.parseInt(variable3Field.getText());
+            } catch (Exception ex) {
+                resultLabel.setText("An error occurred: " + ex.getMessage() + "; try again.");
             }
 
+            try {
+            if (!variable1Field.getText().isEmpty() && !variable2Field.getText().isEmpty() && !variable3Field.getText().isEmpty() && selectedOption.equals("Spanish")) {
+                selectedValue = spanishConjugation(Integer.parseInt(variable2Field.getText()),Integer.parseInt(variable3Field.getText()),variable1Field.getText());
+            } else if (!variable1Field.getText().isEmpty() && !variable2Field.getText().isEmpty() && !variable3Field.getText().isEmpty() && selectedOption.equals("French")) {
+                selectedValue = frenchConjugation(Integer.parseInt(variable2Field.getText()),Integer.parseInt(variable3Field.getText()),variable1Field.getText());
+            } else {
+                selectedValue = "Fill in all fields!";
+            }
+            } catch (Exception ex)
+            {
+                resultLabel.setText("An error occurred: " + ex.getMessage() + "; try again.");
+            }
             resultLabel.setText("Selected variable: " + selectedValue);
         }
+    }
+    public static String spanishConjugation(int personal, int num, String root) {
+        int catgirl;
+        Scanner reader = new Scanner(System.in);
+        if (root.endsWith("ar")) {
+            catgirl = 1;
+        } else if (root.endsWith("er")) {
+            catgirl = 2;
+        } else if (root.endsWith("ir")) {
+            catgirl = 3;
+        } else {
+            catgirl = 0;
+        }
+        root = root.substring(0, root.length() - 2);
+        if (num > 2) {
+            num = 2;
+        }
+        String[][][] ED = {{{"o", "as", "a"}, {"amos", "ais", "an"}}, {{"o", "es", "e"}, {"emos", "eis", "en"}}, {{"o", "es", "e"}, {"imos", "is", "en"}}};
+        root = root + ED[catgirl - 1][num - 1][personal - 1];
+        System.out.println("spn conj out: " + root + ".");
+        return root;
+    }
+    public static String frenchConjugation(int personal, int num, String root) {
+        int catgirl;
+        Scanner reader = new Scanner(System.in);
+        if (root.endsWith("er")) {
+            catgirl = 1;
+        } else if (root.endsWith("ir")) {
+            catgirl = 2;
+        } else if (root.endsWith("re")) {
+            catgirl = 3;
+        } else {
+            catgirl = 0;
+        }
+        root = root.substring(0, root.length() - 2);
+        if (num > 2) {
+            num = 2;
+        }
+        String[][][] ED = {{{"e", "es", "e"}, {"ons", "ez", "ent"}}, {{"is", "is", "it"}, {"issons", "issez", "issent"}}, {{"s", "s", ""}, {"ons", "ez", "ent"}}};
+        root = root + ED[catgirl - 1][num - 1][personal - 1];
+        return root;
     }
 }
